330a460db370675ab9a7ed584f30d350b360ef43
Prateek Narayanan <prateeknarayanan@gmail.com>
2016-08-14 16:26:48 -0700

    Initial commit

---
+++ ValueSwitcher.cpp
@@ -0,0 +1,19 @@
+/*
+ * ValueSwitcher.cpp
+ *
+ *  Created on: Aug 14, 2016
+ *      Author: prateeknarayanan
+ */
+
+#include "ValueSwitcher.h"
+
+int main(int x,int y,int t){
+	t=x;
+	x=y;
+	y=t;
+	return x;
+	return y;
+	return t;
+
+}
+
