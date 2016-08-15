f2d66064016fc5288dc1e61867782ceba883221c
prateeknarayanan <prateeknarayanan@gmail.com>
2016-08-14 18:23:12 -0700

    Update ValueSwitcher.cpp
    Added new variable z

--- ValueSwitcher.cpp
+++ ValueSwitcher.cpp
@@ -7,13 +7,15 @@
 
 #include "ValueSwitcher.h"
 
-int main(int x,int y,int t){
-	t=x;
+int main(int x,int y,int t, int z){
 	x=y;
 	y=t;
+	t=z;
+	z=x;
 	return x;
 	return y;
 	return t;
+	return z;
 
 }
 
